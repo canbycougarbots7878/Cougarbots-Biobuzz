93961c5a34542b33d99fb555e756e7efb3d39a85
Roland_Baker <canbycougar7878@gmail.com>
2026-09-16 16:37:55 -0700

    BREAKING CHANGE TO DriveBase
    omniMove split into drive,strafe,turn functions and will update directions individually

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/DriveBase.java
@@ -15,6 +15,10 @@ public class DriveBase {
     public DcMotor back_left;
     public double power_factor; // All wheel powers will be scaled by this factor
 
+    private double forward = 0;
+    private double right = 0;
+    private double clockwise = 0;
+
     // Constructor
     public DriveBase(HardwareMap hardwareMap)  {
         this.front_right = hardwareMap.get(DcMotor.class, "frontright");
@@ -30,30 +34,18 @@ public class DriveBase {
     }
 
     // Raw wheel control
-    public void setWheelPowers(double Front_Right_Power, double Front_Left_Power, double Back_Right_Power, double Back_Left_Power) {
+    private void setWheelPowers(double Front_Right_Power, double Front_Left_Power, double Back_Right_Power, double Back_Left_Power) {
         this.front_right.setPower(Front_Right_Power);
         this.front_left.setPower(Front_Left_Power);
         this.back_right.setPower(Back_Right_Power);
         this.back_left.setPower(Back_Left_Power);
     }
-    public void stop() {
-        setWheelPowers(0,0,0,0);
-    }
-    public void turn(double speed) {
-        double fl = front_left.getPower() - speed;
-        double fr = front_right.getPower() + speed;
-        double bl = front_left.getPower() - speed;
-        double br = front_left.getPower() + speed;
-        setWheelPowers(fr,fl,br,bl);
-    }
-    public void omniMove(double Forward, double Right, double Rotate) {
-        // THIS HAS BEEN FINICKY
-        double fl = Forward + Right - Rotate;
-        double fr = Forward - Right + Rotate;
-        double bl = Forward - Right - Rotate;
-        double br = Forward + Right + Rotate;
+    private void updateMovement() {
+        double fl = forward + right - clockwise;
+        double fr = forward - right + clockwise;
+        double bl = forward - right - clockwise;
+        double br = forward + right + clockwise;
 
-        // normalize so no value exceeds 1
         double max = Math.max(1.0, Math.max(abs(fl),
                 Math.max(abs(fr), Math.max(abs(bl), abs(br)))));
 
@@ -64,10 +56,32 @@ public class DriveBase {
 
         this.setWheelPowers(fr * power_factor, fl * power_factor, br * power_factor, bl * power_factor);
     }
-    public void omniMoveController(Gamepad gamepad, double speed_mod) {
-        double forward = - gamepad.left_stick_y * speed_mod;
-        double strafe = gamepad.left_stick_x * speed_mod;
-        double turn = - gamepad.right_stick_x * speed_mod;
-        this.omniMove(forward,strafe,turn);
+
+    public void move(double forward, double right, double clockwise) {
+        this.forward = forward;
+        this.right = right;
+        this.clockwise = clockwise;
+        this.updateMovement();
+    }
+    public void drive(double forward) {
+        this.forward = forward;
+        this.updateMovement();
+    }
+    public void strafe(double right) {
+        this.right = right;
+        this.updateMovement();
+    }
+    public void turn(double clockwise) {
+        this.clockwise = clockwise;
+        this.updateMovement();
+    }
+    public void stop() {
+        move(0,0,0);
+    }
+    public void moveWithController(Gamepad gamepad, double speed_mod) {
+        this.forward = - gamepad.left_stick_y * speed_mod;
+        this.right = gamepad.left_stick_x * speed_mod;
+        this.clockwise = - gamepad.right_stick_x * speed_mod;
+        this.updateMovement();
     }
 }
